5a2359de53859a6e5260e24ccf38574c4208a07e
kuma0416 <a24350845@gmail.com>
2019-12-09 00:52:11 +0800

    resTest

--- hello.js
+++ hello.js
@@ -1,4 +1,4 @@
-var express = require("express");
+/*var express = require("express");
 var app = express();
 const port = process.env.PORT || 5000;
 
@@ -7,4 +7,11 @@ app.listen(port, function(req, res){
     console.log("server is running on port:", port);
     res.write("hello world");
     res.end();
-});
+});*/
+var http = require("http");
+
+http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello World");
+  response.end();
+}).listen(8881);
