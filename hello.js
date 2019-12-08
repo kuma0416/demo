67a0a25d3db13c97c01780ff86f11fce7e1309af
kuma0416 <a24350845@gmail.com>
2019-12-09 00:38:28 +0800

    helloTest

---
+++ hello.js
@@ -0,0 +1,8 @@
+var express = require("express");
+var app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, function(){
+    console.log("hello world!");
+    console.log("server is running on port:", port);
+});
