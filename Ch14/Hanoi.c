4dea413e84ad0d0e92a07854c4ce214b8b32de9e
kuma0416 <49488231+kuma0416@users.noreply.github.com>
2020-03-27 10:59:03 +0800

    Some content of ch14 had added

---
+++ Ch14/Hanoi.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void hanoi(int n, char buffer, char src, char dst);
+
+int main(void){
+	int n;
+	scanf("%d", &n);
+	hanoi(n, 'B', 'A', 'C');
+	return 0;
+}
+
+void hanoi(int n, char buffer, char src, char dst){
+	printf("hanoi(%d, %c, %c, %c)\n", n, buffer, src, dst);
+	if(n==1){
+		printf("move disk from %c to %c\n", src, dst);
+	} else {
+		hanoi(n-1, dst, src, buffer);
+		printf("move disk from %c to %c\n", src, dst);
+		hanoi(n-1, src, buffer, dst);
+	}
+}
